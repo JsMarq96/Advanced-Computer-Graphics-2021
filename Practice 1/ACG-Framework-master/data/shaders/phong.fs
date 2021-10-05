8db18fdf0b03916ff72f9166ebfc2f8ad6b0b513
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2021-10-05 21:14:33 +0200

    Update phong.fs

--- Practice 1/ACG-Framework-master/data/shaders/phong.fs
+++ Practice 1/ACG-Framework-master/data/shaders/phong.fs
@@ -22,17 +22,17 @@ void main()
 	vec3 light_vector = normalize(v_position - v_light_local_position);
 
     // Ambient component
-    float ambient_component = u_material_ambient;
+    float ambient_component = u_material_ambient * u_light_ambient;
 
     // Diffuse component
     float light_dot_norm = dot(light_vector, v_normal);
-    float diffuse_componenet = light_dot_norm * u_material_diffuse;
+    float diffuse_componenet = light_dot_norm * u_material_diffuse * u_light_diffuse;
 
     // Specular compomenet
     float reflect_dot_view = dot(v_world_position - u_camera_pos , -normalize(reflect(light_vector, v_normal)));
     reflect_dot_view = clamp(reflect_dot_view, 0.0, 255.0);
 
-    float specular_component = pow(reflect_dot_view, u_material_shininess) * u_material_specular;
+    float specular_component = pow(reflect_dot_view, u_material_shininess) * u_material_specular * u_light_specular;
 
     // Diffuse and ambient lights depend on the base color of the object
     // Specular deppedns on the light color
