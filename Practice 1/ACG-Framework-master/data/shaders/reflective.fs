e267c6e8b35104b4f4fc3f82aa8a70ce52a8c9e5
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2021-10-10 12:00:01 +0200

    Added reflectiveness

--- Practice 1/ACG-Framework-master/data/shaders/reflective.fs
+++ Practice 1/ACG-Framework-master/data/shaders/reflective.fs
@@ -2,7 +2,7 @@ varying vec3 v_position;
 varying vec3 v_world_position;
 varying vec3 v_normal;
 
-uniform vec4 u_color;
+uniform float u_reflectiveness;
 uniform vec3 u_camera_position;
 uniform samplerCube u_texture;
 
@@ -11,5 +11,5 @@ void main()
 	vec3 normal = normalize(v_normal);
     vec3 camera_vec = normalize(v_world_position - u_camera_position);
     // Tint the base texture
-    gl_FragColor = u_color * (textureCube(u_texture, reflect(camera_vec, normal)));
+    gl_FragColor = vec4(u_reflectiveness * textureCube(u_texture, reflect(camera_vec, normal)).xyz, 1.0);
 }
