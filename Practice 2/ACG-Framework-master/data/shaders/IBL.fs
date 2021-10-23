1d3a79a8f27fedf66dcbb762e1c182e821470fbe
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2021-10-23 17:51:08 +0200

    Fixed..? PBR a bit

--- Practice 2/ACG-Framework-master/data/shaders/IBL.fs
+++ Practice 2/ACG-Framework-master/data/shaders/IBL.fs
@@ -45,6 +45,7 @@ struct sMaterial {
     float roughness;
     float metalness;
     vec3 diffuse_color;
+    vec3 base_color;
     float alpha;
     vec3 specular_color;
 };
@@ -71,6 +72,11 @@ vec3 FresnelSchlickRoughness(float cosTheta, vec3 F0, float roughness)
     return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
 }
 
+vec3 fresnelSchlick(float cosTheta, vec3 F0)
+{
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+}
+
 // degamma
 vec3 gamma_to_linear(vec3 color)
 {
@@ -89,11 +95,11 @@ sVectors computeVectors() {
     result.normal = normalize(v_normal);
     result.view = normalize(v_world_position - u_camera_position);
     result.light = normalize(u_light_position - v_world_position);
-    result.reflect = normalize(reflect(result.view, result.normal));
     result.half_v = normalize(result.view + result.light);
-    result.n_dot_v = max(dot(result.normal, result.view), 0.0) + 0.0001;
-    result.n_dot_h = max(dot(result.normal, result.half_v), 0.0) + 0.0001;
-    result.l_dot_n = max(dot(u_light_position, result.normal), 0.0) + 0.0001;
+    result.reflect = normalize(reflect(result.view, result.normal));
+    result.n_dot_v = max(dot(result.normal, result.view), 0.0);
+    result.n_dot_h = max(dot(result.normal, result.half_v), 0.0);
+    result.l_dot_n = max(dot(result.normal, result.light), 0.0) ;
 
     return result;
 }
@@ -103,6 +109,7 @@ sMaterial getMaterialProperties_v1() {
     mat_prop.roughness = texture2D(u_rough_map, v_uv).r;
     mat_prop.metalness = texture2D(u_metal_map, v_uv).r;
     vec4 alb_color = texture2D(u_albedo_map, v_uv);
+    mat_prop.base_color = alb_color.rgb;
     mat_prop.diffuse_color = alb_color.rgb;
     mat_prop.alpha = alb_color.a;
 
@@ -117,6 +124,7 @@ sMaterial getMaterialProperties_v2() {
     mat_prop.roughness = texture2D(u_rough_map, v_uv).g;
     mat_prop.metalness = texture2D(u_rough_map, v_uv).b;
     vec4 alb_color = texture2D(u_albedo_map, v_uv);
+    mat_prop.base_color = alb_color.rgb;
     mat_prop.diffuse_color = alb_color.rgb;
     mat_prop.alpha = alb_color.a;
 
@@ -135,38 +143,46 @@ sMaterial degamma(sMaterial mat) {
 
 // PBR FUNCTIONS
 float normal_Distribution_function(sVectors vects, sMaterial mat_props) {
-    float roughness_squared = mat_props.roughness * mat_props.roughness;
-    float denom = (vects.n_dot_h * vects.n_dot_h) * (roughness_squared - 1.0) + 1.0;
-    float D = roughness_squared / (PI * denom * denom);
-    return D;
+    //return ((mat_props.roughness + 2.0) / (2.0 * PI)) * pow(vects.n_dot_h, mat_props.roughness);
+    float alpha = mat_props.roughness * mat_props.roughness;
+    float alpha_squared = alpha * alpha;
+
+    float denom = ((vects.n_dot_h * vects.n_dot_h) * (alpha_squared - 1.0)) + 1.0;
+    return alpha_squared / (PI * denom * denom);
 }
 
 float Geometry_atenuation_term(sVectors vects, sMaterial mat_props) {
     // Using Epic's aproximation
-    float k = (mat_props.roughness + 1.0);
-    k = k * k;
-    k = k / 8.0;
-    float G1 = vects.n_dot_v / (vects.n_dot_v *(1.0 - k) + k);
-    float G2 = dot(vects.normal, vects.half_v) / (vects.l_dot_n * (1.0 - k) + k);
+    float r = mat_props.roughness + 1.0;
+    float k = (r * r) / 8.0;
+    float G1 = vects.n_dot_v / (vects.n_dot_v * (1.0 - k) + k);
+    float G2 = vects.l_dot_n / (vects.l_dot_n * (1.0 - k) + k);
     return G1 * G2;
 }
 
 vec3 getPixelColor(sVectors vects, sMaterial mat_props) {
     // PBR
     // BRDF Diffuse: Lambertian
-    vec3 diffuse_contribution = mat_props.diffuse_color / PI;
+    vec3 diffuse_contribution = mat_props.diffuse_color;
 
     // BRDF Specular: Cook-Torrance
-    vec3 fresnel = FresnelSchlickRoughness(vects.l_dot_n, mat_props.specular_color, mat_props.roughness);
-    float normalization_factor = 4.0 * vects.n_dot_v * vects.l_dot_n;
+    //vec3 fresnel = FresnelSchlickRoughness(vects.n_dot_v, vec3(1.0), mat_props.roughness);
+    vec3 fresnel = fresnelSchlick(vects.n_dot_h, mat_props.specular_color);
+    float normalization_factor = 4.0 * vects.n_dot_v * vects.l_dot_n + 0.0001;
     
     vec3 specular_contribution = (fresnel * Geometry_atenuation_term(vects, mat_props) * normal_Distribution_function(vects, mat_props)) / normalization_factor;
 
+    vec3 BRDF = diffuse_contribution + specular_contribution;
+
     vec3 radiance = u_light_radiance.rgb;
 
+    //return fresnel;
+    //return vec3(1.0) * normal_Distribution_function(vects, mat_props); //Geometry_atenuation_term(vects, mat_props);
+    return BRDF * vects.l_dot_n;
+    //return diffuse_contribution * vects.l_dot_n;
     //return fresnel * Geometry_atenuation_term(vects, mat_props);
-    return specular_contribution;
-    return diffuse_contribution * specular_contribution * radiance * vects.l_dot_n;
+    //return specular_contribution;
+    return BRDF * radiance * vects.l_dot_n;
 
     // IBL
     /*vec2 LUT_brdf = texture2D(u_brdf_LUT, vec2(vects.n_dot_v, mat_props.roughness)).rg;
@@ -204,7 +220,7 @@ void main() {
     if (u_output_mode == 0.0) {
         output_color = color;
     } else if (u_output_mode == 1.0) {
-        output_color = frag_material.diffuse_color;
+        output_color = frag_material.base_color;
     } else if (u_output_mode == 2.0) {
         output_color = vec3(frag_material.roughness, frag_material.roughness, frag_material.roughness);
     } else if (u_output_mode == 3.0) {
